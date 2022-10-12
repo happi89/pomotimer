ff2403b3fe48e39529f2f1a8c9afcf2679dc2091
happi89 <farhaanp99@gmail.com>
2022-10-12 14:31:33 -0400

    simplified timer component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -13,60 +13,49 @@ export default function Timer(props: PaperProps) {
 		shallow
 	);
 
+	const time = [
+		{
+			label: 'pomodoro',
+			value: pomodoro,
+		},
+		{
+			label: 'short',
+			value: short,
+		},
+		{
+			label: 'long',
+			value: long,
+		},
+	];
+
 	return (
 		<Paper radius='sm' p='xl' shadow='xs' withBorder {...props}>
 			<Stack align='center'>
 				<Tabs defaultValue='pomodoro' variant='outline'>
 					<Tabs.List>
 						<Tabs.Tab value='pomodoro'>Pomodoro</Tabs.Tab>
-						<Tabs.Tab value='short-break'>Short Break</Tabs.Tab>
-						<Tabs.Tab value='long-break'>Long Break</Tabs.Tab>
+						<Tabs.Tab value='short'>Short Break</Tabs.Tab>
+						<Tabs.Tab value='long'>Long Break</Tabs.Tab>
 					</Tabs.List>
 
-					<Tabs.Panel value='pomodoro' pt='xs'>
-						<Stack align='center'>
-							<Title
-								order={1}
-								sx={{
-									fontSize: '4rem',
-								}}>
-								{pomodoro}:00
-							</Title>
-							<Button size='xl' rightIcon={<PlayIcon width={24} />}>
-								START
-							</Button>
-						</Stack>
-					</Tabs.Panel>
-
-					<Tabs.Panel value='short-break' pt='xs'>
-						<Stack align='center'>
-							<Title
-								order={1}
-								sx={{
-									fontSize: '4rem',
-								}}>
-								{short}:00
-							</Title>
-							<Button size='xl' rightIcon={<PlayIcon width={24} />}>
-								START
-							</Button>
-						</Stack>
-					</Tabs.Panel>
-
-					<Tabs.Panel value='long-break' pt='xs'>
-						<Stack align='center'>
-							<Title
-								order={1}
-								sx={{
-									fontSize: '4rem',
-								}}>
-								{long}:00
-							</Title>
-							<Button size='xl' rightIcon={<PlayIcon width={24} />}>
-								START
-							</Button>
-						</Stack>
-					</Tabs.Panel>
+					{time.map((t, i) => {
+						return (
+							<Tabs.Panel value={t.label} pt='xs' key={i}>
+								<Stack align='center'>
+									<Title
+										order={1}
+										sx={{
+											fontSize: '4rem',
+										}}>
+										{t.value}:00
+									</Title>
+									<Button size='xl' rightIcon={<PlayIcon width={24} />}>
+										START
+									</Button>
+								</Stack>
+							</Tabs.Panel>
+						);
+					})}
 				</Tabs>
 			</Stack>
 		</Paper>
