b9a4710c52786993714c32c1f8acbdea0ee90fae
happi89 <farhaanp99@gmail.com>
2022-10-12 08:10:22 -0400

    commit

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -4,34 +4,45 @@ import { useForm } from '@mantine/form';
 import Modal from './Modal';
 import { useTimerStore } from '../pages';
 import { closeAllModals } from '@mantine/modals';
+import { useState } from 'react';
 
 export default function SettingsModal({ matches }: { matches: boolean }) {
 	const { pomodoro, short, long } = useTimerStore((state) => state.time);
 	const changeTimer = useTimerStore((state) => state.changeTimer);
-	const form = useForm({
-		initialValues: {
-			pomodoro: pomodoro,
-			short: short,
-			long: long,
-		},
-	});
+	// const form = useForm({
+	// 	initialValues: {
+	// 		pomodoro: pomodoro,
+	// 		short: short,
+	// 		long: long,
+	// 	},
+	// });
 
 	const inputs = [
 		{
 			label: 'Pomodoro',
 			name: 'pomodoro',
+			value: pomodoro,
+			defaultValue: pomodoro,
 		},
 		{
 			label: 'Short Break',
 			name: 'short',
+			value: short,
+			defaultValue: short,
 		},
 		{
 			label: 'Long Break',
 			name: 'long',
+			value: long,
+			defaultValue: long,
 		},
 	];
 
-	console.log(form.values, 'form values');
+	// console.log(form.values, 'form values');
+	// form.onSubmit((values) => {
+	// 	console.log(values, 'onsubmit values');
+	// 	// closeAllModals();
+	// });
 
 	return (
 		<Modal
@@ -39,11 +50,7 @@ export default function SettingsModal({ matches }: { matches: boolean }) {
 			openButton={matches ? 'Settings' : <Cog6ToothIcon width={20} />}
 			leftIcon={<Cog6ToothIcon width={18} />}>
 			<h4>Time (minutes)</h4>
-			<form
-				onSubmit={form.onSubmit((values) => {
-					console.log(values, 'onsubmit values');
-					// closeAllModals();
-				})}>
+			<form onSubmit={() => console.log('')}>
 				<Grid mb='xl'>
 					{inputs.map((input, i) => {
 						return (
@@ -53,7 +60,10 @@ export default function SettingsModal({ matches }: { matches: boolean }) {
 									min={1}
 									stepHoldDelay={500}
 									stepHoldInterval={100}
-									{...form.getInputProps(input.name)}
+									defaultValue={input.defaultValue}
+									value={input.value}
+									onChange={(value) => changeTimer(value || input.defaultValue)}
+									// {...form.getInputProps(input.name)}
 								/>
 							</Grid.Col>
 						);
